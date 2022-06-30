27f6e2c2cf90294cdb5d03203000b4606e812f61
wjdalstj37 <jms6020@gmail.com>
2022-06-30 17:01:02 +0900

    variable learn

---
+++ script.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "minseo";
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "Jeong minseo"
+
+console.log("your new name is " + myName);
