2eba749ea0b5a9fb265c5817d5b3cbbd24d8d1d2
wjdalstj37 <jms6020@gmail.com>
2022-06-30 17:11:56 +0900

    null, undefined

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const a = 5;
-const b = 2;
-let myName = "minseo";
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "Jeong minseo"
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat); // undefined null
